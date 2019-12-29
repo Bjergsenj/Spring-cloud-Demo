7f3945bc99f49f6b8a9fcd0ae3a6f3108cc47f77
聂明欣 <2424629998@qq.com>
2019-12-29 09:51:44 +0800

    集成spring boot admin

---
+++ actuator-server/src/test/java/com/example/actuator/OauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actuator;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorServerApplication {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
